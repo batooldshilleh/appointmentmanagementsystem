6bc932d6ad019cb203bae1c1ba3d36a59b95a6b8
hp <s11924677>
2022-10-07 23:12:07 +0300

    step definition

--- Music_Institute/src/test/java/pak/proj/Test_login.java
+++ Music_Institute/src/test/java/pak/proj/Test_login.java
@@ -5,73 +5,35 @@ import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
 
-
-
-
-
 public class Test_login {
-	@Given("The user has entered all the data correctly  email, password")
-	public void the_user_has_entered_all_the_data_correctly_email_password1() {
-	    
+user u = new user();
+	
+	@Given("that the admin is not logged in")
+	public void that_the_admin_is_not_logged_in() {
+	    // Write code here that turns the phrase above into concrete actions
+		 u.setLogstat(false);
+         assertFalse(u.getLogstat());
 	}
 
-	@When("user clicks login button")
-	public void user_clicks_login_button() {
+	@Given("The user has entered all the data correctly  {string},{string}")
+	public void the_user_has_entered_all_the_data_correctly(String string, String string2) {
 	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
+	    u.setUsrname(string);
+	    u.setPass(string2);
+	    u.loginCH(string,string2);
 	}
 
-	@Then("go to home page")
-	public void go_to_home_page() {
+	@When("the Type {string}")
+	public void the_type(String string) {
 	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
+	    u.setType(string);
+	    u.TypeCH(string);
 	}
 
-	@When("The user left one of the fields blank  Password , Email")
-	public void the_user_left_one_of_the_fields_blank_password_email() {
+	@Then("show tehar menu")
+	public void show_tehar_menu() {
 	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
-	}
-
-	@When("user clicks login1 button")
-	public void user_clicks_login1_button() {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
-	}
-
-	@Then("Show {string} message.")
-	public void show_message(String string) {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
-	}
-
-	@Given("The user has entered all the data correctly email, password")
-	public void the_user_has_entered_all_the_data_correctly_email_password() {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
-	}
-
-	@Given("Emily is not in the database")
-	public void emily_is_not_in_the_database() {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
-	}
-
-	@When("user clicks register button")
-	public void user_clicks_register_button() {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
-	}
-
-	@Given("password is not in the database")
-	public void password_is_not_in_the_database() {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
-	}
-
-	@When("user clicks register1 button")
-	public void user_clicks_register1_button() {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
+	    u.setLogstat(true);
+	    assertTrue( u.getLogstat());
 	}
 }
