11add50457aa7edd9d146926432b4ddb6bb0ff9b
manar <s11924677>
2022-10-17 15:20:33 +0300

    schedule feature

---
+++ Music_Institute/src/test/java/pak/proj/schedule_Test.java
@@ -0,0 +1,84 @@
+package pak.proj;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+
+public class schedule_Test {
+	@Given("the table of artical")
+	public void the_table_of_artical(io.cucumber.datatable.DataTable dataTable) {
+	    // Write code here that turns the phrase above into concrete actions
+	    // For automatic transformation, change DataTable to one of
+	    // E, List<E>, List<List<E>>, List<Map<K,V>>, Map<K,V> or
+	    // Map<K, List<V>>. E,K,V must be a String, Integer, Float,
+	    // Double, Byte, Short, Long, BigInteger or BigDecimal.
+	    //
+	    // For other transformations you can register a DataTableType.
+	    throw new io.cucumber.java.PendingException();
+	} 
+	
+	@Given("The user chose the class")
+	public void the_user_chose_the_class() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Given("There is no conflict")
+	public void there_is_no_conflict() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Given("The premium paid is sufficient")
+	public void the_premium_paid_is_sufficient() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Given("Number of studint in class less than {int}")
+	public void number_of_studint_in_class_less_than(Integer int1) {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("the article is registered")
+	public void the_article_is_registered() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Given("There is conflict")
+	public void there_is_conflict() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("the show masseg \\({string})")
+	public void the_show_masseg(String string) {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Given("The premium paid not enough")
+	public void the_premium_paid_not_enough() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("the show masseg1 \\({string})")
+	public void the_show_masseg1(String string) {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Given("The number of student more than {int}")
+	public void the_number_of_student_more_than(Integer int1) {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("the show masseg \\({string}).")
+	public void the_show_masseg2(String string) {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+}
